fee425e36de6c75e5bfa7c65ed79df98c8abc554
esraahisham753 <esraahisham753@gmail.com>
2024-05-06 10:35:43 +0000

    Try different templates and integrate tailwindcss

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 import classes from "./style.module.css";
 
+console.log('Server is running in ' + import.meta.env.MODE);
+console.log(import.meta.env);
+console.log(import.meta.env.VITE_SOME_KEY);
+
+if (import.meta.env.DEV) {
+    console.log('This is a development server');
+}
+
 document.querySelector("h1").className = classes.title;
